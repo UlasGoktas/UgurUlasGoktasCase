53ea1c640c179e80414d65d8d2443e6d34dfb421
Ulas Goktas <u.ulas.goktas@gmail.com>
2024-08-28 00:02:58 +0300

    Product Model

---
+++ UgurUlasGoktasCase/Model/Product/Product.swift
@@ -0,0 +1,18 @@
+//
+//  Product.swift
+//  UgurUlasGoktasCase
+//
+//  Created by Ulas Goktas on 27.08.2024.
+//
+
+struct Product: Codable {
+    let brand: String?
+    let createdAt: String?
+    let description: String?
+    let id: String?
+    let image: String?
+    let model: String?
+    let name: String?
+    let price: String?
+    var quantity: Int?
+}
