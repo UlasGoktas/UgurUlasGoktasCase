a56118ebb9db4553e13d4be65dbf64a9e4372b69
Ulas Goktas <u.ulas.goktas@gmail.com>
2024-08-28 20:12:58 +0300

    Notification+Extension

--- UgurUlasGoktasCase/Extensions/Notification+Extension.swift
+++ UgurUlasGoktasCase/Extensions/Notification+Extension.swift
@@ -9,6 +9,9 @@ import Foundation
 
 extension Notification.Name {
     static let cartDBChanged = Notification.Name("CartDBChanged")
-    static let didStartLoading = Notification.Name("didStartLoading")
-    static let didFinishLoading = Notification.Name("didFinishLoading")
+    static let showLoading = Notification.Name("showLoading")
+    static let hideLoading = Notification.Name("hideLoading")
+    static let filtersApplied = Notification.Name("filtersApplied")
+    static let productListUpdateFavoriteDB = Notification.Name("productListUpdateFavoriteDB")
+    static let favoriteListUpdateFavoriteDB = Notification.Name("favoriteListUpdateFavoriteDB")
 }
