7a9c63d2b2aab436c95c10dbebbd17056f5255cb
Ulas Goktas <u.ulas.goktas@gmail.com>
2024-08-28 20:11:19 +0300

    TabBarViewController refactor

--- UgurUlasGoktasCase/Screens/TabBar/TabBarViewController.swift
+++ UgurUlasGoktasCase/Screens/TabBar/TabBarViewController.swift
@@ -9,10 +9,10 @@ import UIKit
 
 final class TabBarViewController: UITabBarController {
     let viewModel = TabBarViewModel()
-    let productListViewController = ProductListViewController(viewModel: ProductListViewModel())
-    let cartViewController = CartViewController(viewModel: CartViewModel())
-    let favoritesViewController = FavoritesViewController(viewModel: FavoritesViewModel())
-    let profileViewController = ProfileViewController(viewModel: ProfileViewModel())
+    let productListViewController = UINavigationController(rootViewController: ProductListViewController())
+    let cartViewController = UINavigationController(rootViewController: CartViewController())
+    let favoriteListViewController = UINavigationController(rootViewController: FavoriteListViewController())
+    let profileViewController = UINavigationController(rootViewController: ProfileViewController())
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -24,7 +24,7 @@ final class TabBarViewController: UITabBarController {
     private func prepareTabBarItems() {
         productListViewController.tabBarItem = UITabBarItem(title: .empty, image: UIImage(systemName: SystemImages.house.rawValue), tag: 0)
         cartViewController.tabBarItem = UITabBarItem(title: .empty, image: UIImage(systemName: SystemImages.cart.rawValue), tag: 1)
-        favoritesViewController.tabBarItem = UITabBarItem(title: .empty, image: UIImage(systemName: SystemImages.star.rawValue), tag: 2)
+        favoriteListViewController.tabBarItem = UITabBarItem(title: .empty, image: UIImage(systemName: SystemImages.star.rawValue), tag: 2)
         profileViewController.tabBarItem = UITabBarItem(title: .empty, image: UIImage(systemName: SystemImages.person.rawValue), tag: 3)
     }
     
@@ -32,7 +32,7 @@ final class TabBarViewController: UITabBarController {
         viewControllers = [
             productListViewController,
             cartViewController,
-            favoritesViewController,
+            favoriteListViewController,
             profileViewController
         ]
     }
