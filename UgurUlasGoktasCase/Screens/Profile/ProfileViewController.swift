316312e4966b339c051147a56424b2de19c56b07
Ulas Goktas <u.ulas.goktas@gmail.com>
2024-08-28 20:11:40 +0300

    Profile Screen

---
+++ UgurUlasGoktasCase/Screens/Profile/ProfileViewController.swift
@@ -0,0 +1,10 @@
+//
+//  ProfileViewController.swift
+//  UgurUlasGoktasCase
+//
+//  Created by Ulas Goktas on 27.08.2024.
+//
+
+import Foundation
+
+final class ProfileViewController: BaseViewController {}
---
+++ UgurUlasGoktasCase/Screens/Profile/ProfileViewModel.swift
@@ -0,0 +1,10 @@
+//
+//  ProfileViewModel.swift
+//  UgurUlasGoktasCase
+//
+//  Created by Ulas Goktas on 27.08.2024.
+//
+
+import Foundation
+
+final class ProfileViewModel: BaseViewModel {}
